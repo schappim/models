6533a9414b9f085b3f834999e9c98aa92a83623a
Marcus Schappi <marcus@schappi.com>
2017-01-20 15:46:23 +1100

    add collection

--- category.rb
+++ category.rb
@@ -8,3 +8,22 @@ class Category
   
   timestamps!
 end
+
+# Collection for Chirpy
+class Collection
+  include MongoMapper::Document
+  key :title, String
+  key :handle, String
+  key :description, String
+  many :sub_collections
+  timestamps!
+end
+
+class SubCollection
+  include MongoMapper::Document
+  key :title, String
+  key :handle, String
+  key :description, String
+  belongs_to :sub_collections
+  timestamps!
+end
